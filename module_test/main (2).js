26f096c7875afd432916be96cb2238d43b18c830
bbi <220047vd@yse-c.net>
2023-10-04 12:08:49 +0900

    fixed

--- module_test/main (2).js
+++ module_test/main (2).js
@@ -1,7 +1,6 @@
-//module
+// モジュールの読み込み
 const itemModule = require('./items')
 const controlModule = require('./control')
 
 var items = itemModule.items
-controlModule.show(items)
 controlModule.show(items)
